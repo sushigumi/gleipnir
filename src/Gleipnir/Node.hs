db92e5b6dfe349ab3a2c875be2d2b66a7ff436fc
sushigumi <osushi.g@gmail.com>
2023-11-18 16:38:45 +1100

    Setup concurrency

--- src/Gleipnir/Node.hs
+++ src/Gleipnir/Node.hs
@@ -2,6 +2,8 @@
 
 module Gleipnir.Node where
 
+import Control.Concurrent (forkIO)
+import Control.Concurrent.Chan (newChan, readChan, writeChan, Chan)
 import Control.Monad (forever, when)
 import Control.Monad.State (StateT, evalStateT, get, lift, put)
 import Data.Aeson (FromJSON, ToJSON, encode)
@@ -14,11 +16,9 @@ import Data.Text.IO (getLine, hPutStrLn)
 import Gleipnir.Message (Message (..), MessageBody, body, getInitNodeID, load)
 import System.IO (BufferMode (LineBuffering), hPutStr, hPutStrLn, hSetBuffering, stderr, stdout)
 
-
 class (MessageBody b) => Node a b | a -> b where
   genResponseBody :: a -> b -> b
   updateState :: a -> b -> a
-
 
 readMessage :: (MessageBody a) => IO (Maybe (Message a))
 readMessage =
@@ -40,23 +40,26 @@ genResponse node (Message src dst body) = Message dst src responseBody
   where
     responseBody = genResponseBody node body
 
-run :: (Node a b, MessageBody b) => StateT a IO ()
-run = forever $ do
-  node <- get
-  message <- lift readMessage
-  when (isJust message) $ do
-    let request = fromJust message
+run :: (Node a b, MessageBody b) => Chan (Message b) -> StateT a IO ()
+run ch = do 
+  lift . forkIO $ forever $ do
+    message <- readMessage
+    when (isJust message) $ do
+      writeChan ch (fromJust message)
+
+  forever $ do
+    node <- get
+    request <- lift (readChan ch)
     put (updateState node (body request))
     let response = genResponse node request
     lift . printReply . encode $ response
     return ()
 
--- Unfortunately, the update function must be here rather than as a method in the Node typeclass
--- because Haskell needs some way to tell it's type
 start :: (Node a b, MessageBody b) => a -> IO ()
 start startState =
   do
     hSetBuffering stdout LineBuffering
     hSetBuffering stderr LineBuffering
     System.IO.hPutStrLn stderr "Starting server"
-    evalStateT run startState
+    ch <- newChan
+    evalStateT (run ch) startState
